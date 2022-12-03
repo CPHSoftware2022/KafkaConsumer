538d629ac639ec387727fd5e8f4b64670e1fab62
MiaSimone <cph-md291@cphbusiness.dk>
2022-12-03 14:54:15 +0100

    started the consumer project

---
+++ Kafka-Consumer/src/main/java/com/example/kafkaconsumer/consumer/ConsumerService.java
@@ -0,0 +1,22 @@
+package com.example.kafkaconsumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class ConsumerService
+{
+      private static final Logger logger = LoggerFactory.getLogger(ConsumerService.class);
+      
+      @KafkaListener(topics = {"customer-topic", "another-test-topic"}, groupId = "exam-project")
+      public void consume(String message) throws IOException
+      {
+            System.out.println("Consumed message:" + message);
+            logger.info("&&& Message [{}] consumed", message);
+      }
+}
+      
